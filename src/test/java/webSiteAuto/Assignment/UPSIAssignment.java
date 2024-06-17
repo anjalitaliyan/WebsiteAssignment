a1ff25bed2f28a8bd99657c95a8aaf37972c4ea4
Pratap Rathi <prataprathi97@gmail.com>
2024-06-18 00:29:53 +0530

    Added logic to handle date & time calender

--- src/test/java/webSiteAuto/Assignment/UPSIAssignment.java
+++ src/test/java/webSiteAuto/Assignment/UPSIAssignment.java
@@ -1,17 +1,11 @@
 package webSiteAuto.Assignment;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UPSIAssignment extends UtilClass
 {
@@ -44,20 +38,82 @@ public class UPSIAssignment extends UtilClass
 		String Recipient = "Mr. Parikh sec";
 		String RecipientEmail = "test@test.in";
 		String UPSI_Nature = "Financial Audit";
+		String UPI_Details = "This is Financial Audit report";
+		String UPI_Purpose = "For acknowledgement";
+		String Sharing_Mode = "Shared via Email";
+		String Date_String = "08-Dec-2022";
+		String Time_String = "23:30";
+		
 		
 
 		// Adding Sharer
-		driver.findElement(By.xpath("//div[@class='md-radio']/input[@id='upsiSharer']/../label")).click();
-		AddingSharerAndRecipient(Sharer, SharerEmail, true);
+//		driver.findElement(By.xpath("//div[@class='md-radio']/input[@id='upsiSharer']/../label")).click();
+//		AddingSharerAndRecipient(Sharer, SharerEmail, true);
 
 
 		// Adding Recipient
-		driver.findElement(By.xpath("//div[@class='md-radio']/input[@id='govevaUser1']/../label")).click();
-		AddingSharerAndRecipient(Recipient, RecipientEmail, false);	
-		Select UPSI_Nature_dropdown = new Select(driver.findElement(By.id("NatureId")));
-		UPSI_Nature_dropdown.selectByVisibleText(UPSI_Nature);
+//		driver.findElement(By.xpath("//div[@class='md-radio']/input[@id='govevaUser1']/../label")).click();
+//		AddingSharerAndRecipient(Recipient, RecipientEmail, false);	
+//		Select UPSI_Nature_dropdown = new Select(driver.findElement(By.id("NatureId")));
+//		UPSI_Nature_dropdown.selectByVisibleText(UPSI_Nature);
+		
+		//Adding textbox details
+		driver.findElement(By.xpath("(//textarea[contains(@class, ng-pristine)])[1]")).sendKeys(UPI_Details);
+		driver.findElement(By.xpath("(//textarea[contains(@class, ng-pristine)])[2]")).sendKeys(UPI_Purpose);
+		driver.findElement(By.xpath("(//div//textarea[contains(@class, ng-pristine)])[3]")).sendKeys(Sharing_Mode);
+		
+		// Handling Calender Dates
+		String[] date_temp = Date_String.split("-");
+		int date = Integer.parseInt(date_temp[0]);
+		String month = date_temp[1];
+		String year = date_temp[2];
+		driver.findElement(By.xpath("//input[@name='sharerDate']")).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ngb-datepicker")));
+		Select Year_Dropdown = new Select(driver.findElement(By.xpath("//ngb-datepicker-navigation-select/select[2]")));
+		Select Month_Dropdown = new Select(driver.findElement(By.xpath("//ngb-datepicker-navigation-select/select[1]")));
+		Year_Dropdown.selectByVisibleText(year);
+		Month_Dropdown.selectByVisibleText(month);
+		String date_select_xpath = "//ngb-datepicker//div[@class='btn-light' and text()='" + date + "']";
+		driver.findElement(By.xpath(date_select_xpath)).click();
+		
+		// Handling Time input
+		String[] time_temp = Time_String.split(":");
+		String hour = time_temp[0];
+		String minutes = time_temp[1];
+		String AM_PM = "AM";
+		if(Integer.parseInt(hour)>12) {
+			AM_PM = "PM";
+			hour = String.valueOf(Integer.parseInt(hour)-12);
+		}
+		driver.findElement(By.xpath("(//input[contains(@class,'timepicker-init')])[1]")).click();
+		
+		WebElement am_pm_selector = driver.findElement(By.xpath("//button[@title='Toggle Period']"));
+		WebElement increment_hour = driver.findElement(By.xpath("//a[@title='Increment Hour']"));
+		WebElement increment_minute = driver.findElement(By.xpath("//a[@title='Increment Minute']"));
+		WebElement hour_text = driver.findElement(By.xpath("//span[@title='Pick Hour']"));
+		WebElement minute_text = driver.findElement(By.xpath("//span[@title='Pick Minute']"));
+		boolean loop = true;
+		while (loop) {
+			if(hour_text.getText().equals(hour) && minute_text.getText().equals(minutes) && am_pm_selector.getText().equals(AM_PM)) {
+				loop = false;
+			}
+			if(!hour_text.getText().equals(hour)) {
+				increment_hour.click();
+			}
+			if(!minute_text.getText().equals(minutes)) {
+				increment_minute.click();
+			}
+			if(!am_pm_selector.getText().equals(AM_PM)) {
+				am_pm_selector.click();
+			}
+		}
+		
+		
+		
 
 	}
+	
+	
 	
 	public static void AddingSharerAndRecipient(String name, String email, Boolean sender) {
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ngb-modal-window[@role='dialog']")));
@@ -70,18 +126,7 @@ public class UPSIAssignment extends UtilClass
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[@class='md-checkbox']/label)[1]"))).click();
 		driver.findElement(By.xpath("//ngb-modal-window//div[@class='modal-footer']/button[1]")).click();
 		
-		//calling placeholder method
-		Placeholder();
 	}
 	
-	public static void Placeholder() 
-	{
-		String Placeholder1="Adding details in first placeholder";
-		String Placeholder2="Adding details in second placeholder";
-		String Placeholder3="Adding details in third placeholder";
-		driver.findElement(By.xpath("(//textarea[contains(@class, ng-pristine)])[1]")).sendKeys(Placeholder1);
-		driver.findElement(By.xpath("(//textarea[contains(@class, ng-pristine)])[2]")).sendKeys(Placeholder2);
-		driver.findElement(By.xpath("(//textarea[contains(@class, form-control)])[5]")).sendKeys(Placeholder3);
-	}
 
 }
